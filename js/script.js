8341a72a2aaa1b37debce4829176c9fc5f52b1a8
RenderForge Studio <antimonio5elurio@gmail.com>
2026-05-03 21:00:41 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@ const proyectos = [
     {
         titulo: "Half-Life 3D Game",
         desc: "Diseño de un cuarto minimalista.",
-        imagen: "https://cdnb.artstation.com/p/assets/images/images/098/477/277/large/artissst-blend-render1.jpg",
+        imagen: "https://cdnb.artstation.com/p/assets/images/images/098/477/277/large/artissst-blend-render1.jpg?1777185857",
         specs: "RENDER: CYCLES"
     },
     {
         titulo: "Patrulla Low Poly",
         desc: "Vehículo optimizado GameAsset.",
-        imagen: "https://cdnb.artstation.com/p/assets/images/images/098/709/995/large/artissst-blend-4.jpg",
+        imagen: "https://cdnb.artstation.com/p/assets/images/images/098/709/995/large/artissst-blend-4.jpg?1777853229",
         specs: "STYLE: LOW POLY"
     },
     {
         titulo: "Portal Game Room",
         desc: "Cuarto realista en 3D inspirado en Portal.",
-        imagen: "https://cdna.artstation.com/p/assets/images/images/098/452/908/large/artissst-blend-asdaf.jpg",
+        imagen: "https://cdna.artstation.com/p/assets/images/images/098/452/908/large/artissst-blend-asdaf.jpg?1777089086",
         specs: "Cycle"
     }
 ];
